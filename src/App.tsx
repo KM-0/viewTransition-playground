dab1de28f17a77792d309cfdbc94beb96af6d3d0
Keita Maeda <laughlaughlife@outlook.jp>
2024-06-12 00:57:56 +0900

    動作確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
 import PostDetail from "./pages/PostDetail";
+import Page2 from "./pages/Page2";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/page2" element={<Page2 />} />
         <Route path="/post/:id" element={<PostDetail />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Page2/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.css";
+
+const Page2 = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Page2 Page</h1>
+      <a href={"/"}>Go to Home</a>
+    </div>
+  );
+};
+
+export default Page2;
