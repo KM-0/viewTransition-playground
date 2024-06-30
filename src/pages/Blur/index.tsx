44d49229a6ae94a3c04d3ebecb49e26def328fcf
Keita Maeda <laughlaughlife@outlook.jp>
2024-06-30 22:30:36 +0900

    chore

---
+++ src/pages/Blur/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.css";
+
+const Blur = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <p className={styles.text}>blurかけてみた</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blur;
